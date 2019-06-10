a85f3a1df4deca804b3e804b2ef74979b4a05b73
student <jwang@vsu.edu>
2019-06-10 00:03:00 -0400

    remove build from .gitignore, build is now traced.

---
+++ build/CMakeFiles/GimbalSmall2dPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GimbalSmall2dPlugin.dir/src/GimbalSmall2dPlugin.cc.o"
+  "libGimbalSmall2dPlugin.pdb"
+  "libGimbalSmall2dPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GimbalSmall2dPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
